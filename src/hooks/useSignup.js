d39ad40961fe4ceb75ea337ab32e59ace9e52394
kevinmarindev <kevin.marin7@hotmail.com>
2022-08-09 14:54:19 -0500

    signup is working and redirects authenticated users to create component

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,3 +1,4 @@
+import { Pending } from '@mui/icons-material';
 import { useState, useEffect } from 'react';
 import { projectAuth, projectStorage, projectFirestore } from '../firebase/config';
 import { useAuthContext } from './useAuthContext';
@@ -11,18 +12,21 @@ export const useSignup = () => {
 	const signup = async (email, password, displayName, thumbnail) => {
 		setError(null);
 		setIsPending(true);
-
+		console.log('at step one', isPending, isCancelled)
 		try {
 			// signup
+			console.log('signing up', isPending, isCancelled)
 			const res = await projectAuth.createUserWithEmailAndPassword(
 				email,
 				password
 			);
 
+			console.log('at step two')
+
 			if (!res) {
 				throw new Error('Could not complete signup');
 			}
-
+			console.log('at step 3')
 			// upload the image to storage after user is created so we have access to the id
 			const uploadPath = `thumbnails/${res.user.uid}/${thumbnail.name}`;
 			const img = await projectStorage.ref(uploadPath).put(thumbnail);
@@ -42,19 +46,27 @@ export const useSignup = () => {
 			dispatch({ type: 'LOGIN', payload: res.user });
 
 			if (!isCancelled) {
+				console.log('successful singup')
 				setIsPending(false);
 				setError(null);
+				console.log(Pending, error)
 			}
 		} catch (err) {
 			if (!isCancelled) {
+				console.log('error in signup')
 				setError(err.message);
 				setIsPending(false);
+				
+			}
+			if(isCancelled){
+				console.log('user navigated away')
+				setError(err.message)
 			}
 		}
 	};
 
 	useEffect(() => {
-		return () => setIsCancelled(true);
+		return () => setIsCancelled(true), console.log('cleaning')
 	}, []);
 
 	return { signup, error, isPending };
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,26 +1,54 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSignup } from '../../hooks/useSignup';
+import { useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../../hooks/useAuthContext';
+
 
 //Styling
 import  Container  from '@mui/material/Container';
 import  Paper  from '@mui/material/Paper';
 import CircularProgress from '@mui/material/CircularProgress'
-import { Typography, TextField, Button, Alert, Box } from '@mui/material';
+import { Typography, TextField, Button, Alert } from '@mui/material';
+import InputAdornment from '@mui/material/InputAdornment';
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import EmailTwoTone from "@mui/icons-material/EmailTwoTone";
+import PersonIcon from '@mui/icons-material/Person';
 
 
 const Signup = () => {
+	
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [displayName, setDisplayName] = useState('');
 	const [thumbnail, setThumbnail] = useState(null);
 	const [thumbError, setThumbError] = useState(null);
+	const [showPassword, setShowPassword] = useState(false)
 
 	const { signup, isPending, error } = useSignup();
+	const { user } = useAuthContext()
+	const nav = useNavigate()
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-		signup(email, password, displayName, thumbnail);
+		if(!thumbnail) return setThumbError('Please select a thumbnail')
+		await signup(email, password, displayName, thumbnail);
+		// console.log(error, isPending, user)
+		// if(user) nav('/create')
 	};
+
+
+	//
+	const toggleShowPass = (e) => {
+		setShowPassword(!showPassword)
+	}
+
+	//check to see if there is an user created and prevents logged in users from visiting signup unless they log out 
+	useEffect(()=>{
+		console.log(user)
+		if(user) nav('/create')
+	}, [user])
+
 
 	const handleFileChange = (e) => {
 		setThumbnail(null);
@@ -48,27 +76,42 @@ const Signup = () => {
 		<Paper component={"form"} id='singup' elevation={10}
 			 sx={{backgroundColor: 'white', 
 			 	height: '75vh', 
-			 	width: '75vw',
+			 	width: {lg: '45vw', md: '65vw', sm: '50vw'},
 				borderRadius: '10px',
 				display:"flex",
-				flexDirection:"column",
-				'& .MuiTextField-root': {width: '50%'}}}>
-			<Typography component={'h2'} variant={'h3'} color='black' py={10}>Sign Up</Typography>
+				flexDirection: "column",
+				alignItems: 'center',
+
+				'& .MuiTextField-root': {width: {xs: '70%', sm: '60%', md: '40%'}, mb: 2}}}>
+			<Typography component={'h2'} variant={'h3'} color='black' pt={10} pb={4}>Sign Up</Typography>
+
 			<TextField required label='Email' id='singup-email' 
 					value={email} onChange={(e) => setEmail(e.target.value)}
+					InputProps={{
+            		startAdornment: <InputAdornment position="start"><EmailTwoTone /></InputAdornment>,
+          			}}
+          			// variant="filled"
 			/>
-			<TextField required label='Password' id='singup-password' type="password"
+			<TextField required label='Password' id='singup-password' type={showPassword ? 'text' : 'password'}
 					value={password} onChange={(e) => setPassword(e.target.value)}
+					InputProps={{
+            		startAdornment: <InputAdornment position="start" onClick={toggleShowPass}>{showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}</InputAdornment>,
+          			}}
 			/>
 			<TextField required label='Display Name' id='singup-name' 
 					value={displayName} onChange={(e) => setDisplayName(e.target.value)}
+					InputProps={{
+            		startAdornment: <InputAdornment position="start"><PersonIcon /></InputAdornment>,
+          			}}
 			/>
 			<TextField required helperText='Choose a Thumbnail' id='singup-thumbnail' type='file' 
-					  error={thumbError} onChange={handleFileChange}
+					  error={Boolean(thumbError)} onChange={handleFileChange}
 			/>
-			{!isPending && <Button size='large' variant='contained' onClick={handleSubmit}>Sign Up</Button>}
-			{isPending && <CircularProgress/>}
+			{/* {!isPending && <Button size='large' variant='contained' onClick={handleSubmit}>Sign Up</Button>} */}
+			
+			{(isPending && !error) ? <CircularProgress/> : <Button size='large' variant='contained' onClick={handleSubmit}>Sign Up</Button>}
 			{error && <Alert  severity='error'>{error}</Alert> }
+			{thumbError && <Alert  severity='error'>{thumbError}</Alert> }
 		
 		</Paper>
 		</Container >

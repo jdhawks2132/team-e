8b20b9289ac600db21dd435f189cfba5334e7ab8
jawacaser <97850241+jawacaser@users.noreply.github.com>
2022-08-14 22:17:05 -0500

    Remove old commented code

--- src/pages/nav/Navbar.jsx
+++ src/pages/nav/Navbar.jsx
@@ -25,14 +25,6 @@ const AppBar = styled(MuiAppBar, {
   }),
 }));
 
-// const DrawerHeader = styled('div')(({ theme }) => ({
-//   display: 'flex',
-//   alignItems: 'center',
-//   padding: theme.spacing(0, 1),
-//   // necessary for content to be below app bar
-//   ...theme.mixins.toolbar,
-//   justifyContent: 'flex-end',
-// }));
 const defaultUser = {displayName: '', photoURL: '', email: ''}
 
 export default function Navbar() {

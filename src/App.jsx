67cd943753b717052ab19c7f7ca1b8abd1205cd6
Joshua Hawks <jdhawks@gmail.com>
2022-08-18 09:06:20 -0500

    clean App.jsx/ remove unused page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuthContext } from './hooks/useAuthContext';
-import reactLogo from './assets/react.svg';
+
 import './App.css';
 
 import Dashboard from './pages/dashboard/Dashboard';
-import Create from './pages/create/Create';
 import Project from './pages/project/Project';
 import Signup from './pages/signup/Signup';
 import Login from './pages/login/Login';
 import NewProject from './pages/create/NewProject';
-import Logout from './pages/logout/Logout';
 import Navbar from './pages/nav/Navbar';
 
 function App() {
@@ -23,18 +20,10 @@ function App() {
 					<Navbar />
 					<Routes>
 						<Route
-							path='/out'
-							element={user ? <Logout /> : <Navigate to='/login' />}
-						/>
-						<Route
 							path='/'
 							element={user ? <Dashboard /> : <Navigate to='/login' />}
 						/>
 						<Route
-							path='/create'
-							element={user ? <Create /> : <Navigate to='/login' />}
-						/>
-            			<Route
 							path='/newproject'
 							element={user ? <NewProject /> : <Navigate to='/login' />}
 						/>
